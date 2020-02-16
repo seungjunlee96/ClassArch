d617e834af45281ab2ee3869e39bb573874c55ed
김영한 <godppkyh@gmail.com>
2020-02-17 02:41:54 +0900

    commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name            = 'ClassARch',
+    version         = '1.0',
+    description     = 'ClassArch: Self-training method for 3D Classification',
+    author          = 'Younghan Kim',
+    author_email    = 'godppkyh@gmail.com',
+    install_requires= [],
+    packages        = find_packages(),
+    python_requires = '>=3.6'  
+)
